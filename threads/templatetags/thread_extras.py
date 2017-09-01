75edb5b7afa7a455a84cbca0533586105e808b68
cormacio100 <cormac.music@gmail.com>
2017-09-02 00:53:00 +0100

    commenting added to custom template tag file

--- threads/templatetags/thread_extras.py
+++ threads/templatetags/thread_extras.py
@@ -9,6 +9,8 @@ register = template.Library()
 #   particular thread, which in turn is associated with a subject
 
 
+#   iterates through the threads contained in a subject and counts
+#   up the number of posts contained in all the threads combined.
 @register.filter
 def get_total_subject_posts(subject):
     total_posts = 0
@@ -17,12 +19,16 @@ def get_total_subject_posts(subject):
     return total_posts
 
 
+#   uses arrow to return a simple ‘humanized’ version of the time,
+#   since the supplied datetime objects time occurred
 @register.filter
 def started_time(created_at):
     #   return a Human Friendly date
     return arrow.get(created_at).humanize()
 
 
+#   we filter all our posts from the supplied thread and order by
+#   the created_at time to find the newest post in the thread, and return the username of that user.
 @register.simple_tag
 def last_posted_user_name(thread):
     #   retrieve the most recent post saved
