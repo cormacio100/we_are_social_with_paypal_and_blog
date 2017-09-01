c20692bd63f267c6523ebf214f696708ad26b038
cormacio100 <cormac.music@gmail.com>
2017-09-01 01:06:24 +0100

    added views to allow creation of new Thread. Incomplete

--- threads/views.py
+++ threads/views.py
@@ -8,6 +8,7 @@ from django.core.urlresolvers import reverse
 from django.template.context_processors import csrf
 from django.shortcuts import render
 from threads.models import Subject
+from .forms import ThreadForm, PostForm
 
 
 def forum(request):
@@ -21,4 +22,44 @@ def threads(request,subject_id):
 
 @login_required
 def new_thread(request,subject_id):
-    pass
+    subject = get_object_or_404(Subject, pk=subject_id)
+    if request.method == 'POST':
+        #   create instances of thread_form and post_form
+        thread_form = ThreadForm(request.POST)
+        post_form = PostForm(request.POST)
+        if thread_form.is_valid() and post_form.is_valid():
+            #   Passing false to save method stops save to DB
+            #   create a memory-only version of the THREAD model
+            #   The reason is to assign the user from request.user
+            #   as the Thread creator before it gets saved
+            thread = thread_form.save(False)
+            thread.subject = subject
+            thread.user = request.user
+            thread.save()
+
+            #   same for POST
+            #   We create a memory-only version of the Post model
+            #   We want to stop it being saved before we can assign
+            #   request.user to it as the user
+            post = post_form.save(False)
+            post.user = request.user
+            post.thread = thread
+            post.save()
+
+            messages.success(request,"You have created a new thread!")
+
+            #   Pass to a view thread view, which is expecting arguments
+            return redirect(reverse('thread',args={thread.pk}))
+    else:
+        #   empty forms
+        thread_form = ThreadForm()
+        post_form = PostForm(request.POST)
+
+    args = {
+        'thread_form' : thread_form,
+        'post_form' : post_form,
+        'subject' : subject,
+    }
+    args.update(csrf(request))
+
+    return render(request,'forum/thread_form.html',args)
