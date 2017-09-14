c65bf20c839a1b7884e94f184d111df3b6a2383b
cormacio100 <cormac.music@gmail.com>
2017-09-14 17:59:37 +0100

    Database connectivity details for heroku added

--- settings/staging.py
+++ settings/staging.py
@@ -1,12 +1,14 @@
 from base import *
+import dj_database_url
 
 DEBUG = False
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
-	}
+	#'default': {
+	#	'ENGINE': 'django.db.backends.sqlite3',
+	#	'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+	#}
+	'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
 
 #	Stripe environment variables
