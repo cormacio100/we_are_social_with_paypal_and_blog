056b85d7604f996179f55a292a549ae4680fecab
cormacio100 <cormac.music@gmail.com>
2017-09-21 00:20:27 +0100

    Updating Database connectivity

--- settings/staging.py
+++ settings/staging.py
@@ -4,11 +4,7 @@ import dj_database_url
 DEBUG = False
 
 DATABASES = {
-	#'default': {
-	#	'ENGINE': 'django.db.backends.sqlite3',
-	#	'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
-	#}
-	'default': dj_database_url.config('CLEARDB_DATABASE_URL')
+   'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
 
 # Stripe environment variables
