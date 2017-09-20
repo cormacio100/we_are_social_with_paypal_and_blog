16dae08841704c25c57a9fee9632fafe46adf558
cormacio100 <cormac.music@gmail.com>
2017-09-20 16:33:48 +0100

    moved allowed host-heroku to staging.py from base

--- settings/staging.py
+++ settings/staging.py
@@ -11,12 +11,13 @@ DATABASES = {
 	'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
 
-#	Stripe environment variables
+# Stripe environment variables
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_nbWefqblVg8HnYsFmpcld8qj')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_N35jP51CRqW4FKBMa8MAL1A4')
 
-#	Paypa;l environment variables
+# Paypal environment variables
 SITE_URL = 'https://code-institute-we-are-social.herokuapp.com/'
+ALLOWED_HOSTS += 'http://code-institute-we-are-social.herokuapp.com/'
 PAYPAL_NOTIFY_URL = 'code-institute-we-are-social.herokuapp.com'	#	your heroku app URL
 PAYPAL_RECEIVER_EMAIL = 'cormac.music@gmail.com'	#	your Paypal merchant email
 
